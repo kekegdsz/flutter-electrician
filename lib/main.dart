373cd17ba734110d400e851f0e5373815dee7207
keke <keke@ky-tech.com.cn>
2024-05-16 17:38:26 +0800

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
   String _selectedCategory = 'Category 1';
 
-  static List<Widget> _widgetOptions = <Widget>[
+  static final List<Widget> _widgetOptions = <Widget>[
     HomeFragment(),
     FavoritesFragment(),
     SettingsFragment(),
